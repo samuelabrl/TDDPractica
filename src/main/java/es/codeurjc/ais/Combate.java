f847b126d31ae62494b0f525a57c8d145a256f0c
Samuel Abraldes <samueljr.abraldes@gmail.com>
2023-03-26 18:59:35 +0200

    Implementación de resultados en el código de combatir refactorizado

--- src/main/java/es/codeurjc/ais/Combate.java
+++ src/main/java/es/codeurjc/ais/Combate.java
@@ -8,17 +8,32 @@ public class Combate {
   }
 
   public static String combatir(Carta atacante, Carta defensor) {
+      boolean destruido = false;
+      boolean perdidaPuntos = false;
       int puntosAtaque = atacante.getAtaque();
       int puntosDefensor = defensor.getAtaque();
       if (defensor.getPosicion() == Posicion.Defensa) {
           puntosDefensor = defensor.getDefensa();
       }
 
-      String resultado = atacante + " vs " + defensor;
-      Carta ganador = ((puntosAtaque > puntosDefensor) ? atacante : defensor);
+      StringBuilder resultado = new StringBuilder();
+      resultado.append(atacante).append(" vs ").append(defensor).append(" -> ");
+      Carta ganador = ((puntosAtaque > puntosDefensor) ? atacante : (puntosAtaque == puntosDefensor) ? null : defensor);
+      Carta perdedor = ((ganador == null) ? null : (atacante == ganador) ? defensor : atacante);
 
-      return "";
+      if (ganador == null) {
+          resultado.append("Empate");
+      }
+      else {
+          resultado.append("Gana carta ").append(ganador.getNombre());
+      }
+
+      return resultado.toString() + ((perdidaPuntos) ? "Hola" : "") + ((destruido) ? "Hola" : "\n");
   }
 
 }
 
+/*  Optional<Carta> ganador2 = Optional.ofNullable((puntosAtaque > puntosDefensor) ? atacante :
+                                                    (puntosAtaque == puntosDefensor) ? null : defensor);
+      resultado.append(ganador2.map(carta -> {return "Gana carta" + carta.getNombre();}).orElseGet(() -> {return "Empate";}));
+ */
