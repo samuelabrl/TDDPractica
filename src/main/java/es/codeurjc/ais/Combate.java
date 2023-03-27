5a9daa94af4cb1ac7fc5aca18025d97d9c65ee99
Samuel Abraldes <samueljr.abraldes@gmail.com>
2023-03-27 04:41:47 +0200

    Estado código antes implementación inmortal al defensor

--- src/main/java/es/codeurjc/ais/Combate.java
+++ src/main/java/es/codeurjc/ais/Combate.java
@@ -13,26 +13,28 @@ public class Combate {
       }
       int puntosAtaque = atacante.getAtaque();
       int puntosDefensor = (defensor.getPosicion() == Posicion.Defensa) ? defensor.getDefensa() : defensor.getAtaque();
+      boolean atacanteInmortal = atacante.getEfecto() == Efecto.Inmortal;
 
       StringBuilder resultado = new StringBuilder();
       resultado.append(String.format("%s vs %s -> ", atacante, defensor));
       Carta ganador = ((puntosAtaque > puntosDefensor) ? atacante : (puntosAtaque == puntosDefensor) ? null : defensor);
       Carta perdedor = ((ganador == null) ? null : (atacante == ganador) ? defensor : atacante);
 
-      if (ganador != null) {
+      if (ganador != null) { // No es empate
           resultado.append(String.format("Gana Carta %s. ", ganador.getNombre()));
           int perdidaVida = Math.abs(puntosAtaque - puntosDefensor);
           if (defensor.getPosicion() == Posicion.Ataque) {
-              resultado.append((ganador == atacante) ? "Defensor " : "Atacante ").append(String.format("pierde %d puntos. ", perdidaVida)).
-              append("Carta ").append(perdedor.getNombre()).append(" destruido/a.");
+              resultado.append((ganador == atacante) ? "Defensor " : "Atacante ").append(String.format("pierde %d puntos.", perdidaVida)).
+                      append((atacanteInmortal && perdedor == atacante) ? "" : String.format(" Carta %s destruido/a.", perdedor.getNombre()));
           }
           else {
               resultado.append((ganador == defensor) ? String.format("Atacante pierde %d puntos", perdidaVida) :
                       String.format("Carta %s destruido/a", defensor.getNombre()));
           }
       }
-      else {
-          resultado.append("Empate.").append((defensor.getPosicion() == Posicion.Ataque) ? " Ambas cartas destruidas." : "");
+      else { // Es empate
+          resultado.append("Empate.").append((defensor.getPosicion() == Posicion.Ataque && !atacanteInmortal) ? " Ambas cartas destruidas." :
+                  (atacanteInmortal) ? String.format(" Carta %s destruido/a.", defensor.getNombre()) : "");
       }
       return resultado.toString();
   }
--- src/test/java/es/codeurjc/ais/CasoIntermedioTest.java
+++ src/test/java/es/codeurjc/ais/CasoIntermedioTest.java
@@ -12,8 +12,32 @@ public class CasoIntermedioTest {
         Carta dos = new Carta("dos", 2000, 1500, Posicion.Ataque);
 
         String resultado= Combate.combatir(uno, dos); //arg1: atacante, arg2: defenso
-        String resultadoEsperado = "Carta uno (2000/2850/Posición: Ataque/Efecto: Inmortal) vs Carta dos (1500/1500/Posición: Ataque/Efecto: N/A) -> " +
+        String resultadoEsperado = "Carta uno (2000/2850/Posición: Ataque/Efecto: Inmortal) vs Carta dos (2000/1500/Posición: Ataque/Efecto: N/A) -> " +
                 "Empate. Carta dos destruido/a.";
+
+        assertEquals(resultadoEsperado, resultado);
+    }
+
+    @Test
+    public void caso9() throws IllegalPositionException {
+        Carta uno = new Carta("uno", 1800, 2850, Posicion.Ataque, Efecto.Inmortal);
+        Carta dos = new Carta("dos", 2000, 1500, Posicion.Ataque);
+
+        String resultado= Combate.combatir(uno, dos); //arg1: atacante, arg2: defenso
+        String resultadoEsperado = "Carta uno (1800/2850/Posición: Ataque/Efecto: Inmortal) vs Carta dos (2000/1500/Posición: Ataque/Efecto: N/A) -> " +
+                "Gana Carta dos. Atacante pierde 200 puntos.";
+
+        assertEquals(resultadoEsperado, resultado);
+    }
+
+    @Test
+    public void caso10() throws IllegalPositionException {
+        Carta uno = new Carta("uno", 2100, 2850, Posicion.Ataque);
+        Carta dos = new Carta("dos", 2000, 1500, Posicion.Ataque, Efecto.Inmortal);
+
+        String resultado= Combate.combatir(uno, dos); //arg1: atacante, arg2: defenso
+        String resultadoEsperado = "Carta uno (2100/2850/Posición: Ataque/Efecto: N/A) vs Carta dos (2000/1500/Posición: Ataque/Efecto: Inmortal) -> " +
+                "Gana Carta dos. Defensor pierde 100 puntos.";
 
         assertEquals(resultadoEsperado, resultado);
     }
