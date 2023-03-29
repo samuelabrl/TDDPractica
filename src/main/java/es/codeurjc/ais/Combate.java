36dfbf915b661385488731b145ad8fb5e8828f90
JuanMAedo <juan.martinez.aedo.g@gmail.com>
2023-03-29 12:50:56 +0200

    hasta caso 28 hecho y refactorizado; comprobar fallo del 29 y 30 y añadir a la memoria estos últimos casos

--- src/main/java/es/codeurjc/ais/Combate.java
+++ src/main/java/es/codeurjc/ais/Combate.java
@@ -2,47 +2,47 @@ package es.codeurjc.ais;
 
 public class Combate {
 
-  private Combate() {
-  }
+    private Combate() {
+    }
 
-  public static String combatir(Carta atacante, Carta defensor) throws IllegalPositionException {
-      if (atacante.getPosicion() == Posicion.DEFENSA) {
-          throw new IllegalPositionException("La carta atacante no puede estar en una posición de Defensa");
-      }
-      int puntosAtaque = atacante.getAtaque();
-      int puntosDefensor = (defensor.getPosicion() == Posicion.DEFENSA) ? defensor.getDefensa() : defensor.getAtaque();
+    public static String combatir(Carta atacante, Carta defensor) throws IllegalPositionException {
+        if (atacante.getPosicion() == Posicion.DEFENSA) {
+            throw new IllegalPositionException("La carta atacante no puede estar en una posición de Defensa");
+        }
+        int puntosAtaque = atacante.getAtaque();
+        int puntosDefensor = (defensor.getPosicion() == Posicion.DEFENSA) ? defensor.getDefensa() : defensor.getAtaque();
 
-      StringBuilder resultado = new StringBuilder();
-      resultado.append(String.format("%s vs %s -> ", atacante, defensor));
-      Carta ganador = ((puntosAtaque > puntosDefensor) ? atacante : (puntosAtaque == puntosDefensor) ? null : defensor);
-      Carta perdedor = ((ganador == null) ? null : (atacante == ganador) ? defensor : atacante);
+        StringBuilder resultado = new StringBuilder();
+        resultado.append(String.format("%s vs %s -> ", atacante, defensor));
+        Carta ganador = ((puntosAtaque > puntosDefensor) ? atacante : (puntosAtaque == puntosDefensor) ? null : defensor);
+        Carta perdedor = ((ganador == null) ? null : (atacante == ganador) ? defensor : atacante);
 
-      if (ganador != null) { // No es empate
-          resultado.append(String.format("Gana Carta %s.", ganador.getNombre()));
-          int perdidaVida = Math.abs(puntosAtaque - puntosDefensor);
-          if (defensor.getPosicion() == Posicion.ATAQUE) {
-              resultado.append((ganador == atacante) ? " Defensor " : " Atacante ").append(String.format("pierde %d puntos.", (perdedor.presion()) ? perdidaVida / 2 : perdidaVida)).
-                      append((perdedor.esInmortal()) ? "" : (atacante.toqueMortal() && defensor.getDefensa() < 2000) ? " Ambas cartas destruidas." :
-                      String.format(" Carta %s destruido/a.", perdedor.getNombre()));
-          }
-          else {
-              resultado.append((ganador == defensor) ? String.format(" Atacante pierde %d puntos.", (perdedor.presion()) ? perdidaVida / 2 : perdidaVida) : "");
-              resultado.append((ganador == atacante && !perdedor.esInmortal() && defensor.toqueMortal() && atacante.getDefensa() < 2000) ? " Ambas cartas destruidas." :
-                      (ganador == atacante && !perdedor.esInmortal() || atacante.toqueMortal() && defensor.getDefensa() < 2000)
-                      ? String.format(" Carta %s destruido/a.", defensor.getNombre()) : "");
-          }
-      }
-      else { // Es empate
-          resultado.append("Empate.");
-          if ((defensor.getPosicion() == Posicion.ATAQUE && !atacante.esInmortal()) || (atacante.toqueMortal() && defensor.toqueMortal() && defensor.getDefensa() < 2000 && atacante.getDefensa() < 2000)) {
-              resultado.append(" Ambas cartas destruidas.");
-          }
-          else if (atacante.esInmortal()) {
-              resultado.append(String.format(" Carta %s destruido/a.", defensor.getNombre()));
-          }
-      }
-      return resultado.toString();
-  }
+        if (ganador != null) { // No es empate
+            resultado.append(String.format("Gana Carta %s.", ganador.getNombre()));
+            int perdidaVida = Math.abs(puntosAtaque - puntosDefensor);
+            if (defensor.getPosicion() == Posicion.ATAQUE) {
+                resultado.append((ganador == atacante) ? " Defensor " : " Atacante ").append(String.format("pierde %d puntos.", (perdedor.presion()) ? perdidaVida / 2 : perdidaVida)).
+                        append((perdedor.esInmortal()) ? "" : (atacante.toqueMortal() && defensor.getDefensa() < 2000 && !ganador.esInmortal()) ? " Ambas cartas destruidas." :
+                                String.format(" Carta %s destruido/a.", perdedor.getNombre()));
+            } else {
+                resultado.append((ganador == defensor) ? String.format(" Atacante pierde %d puntos.", (perdedor.presion()) ? perdidaVida / 2 : perdidaVida) : "");
+                resultado.append((ganador == atacante && !perdedor.esInmortal() && !atacante.esInmortal() && defensor.toqueMortal() && atacante.getDefensa() < 2000) ? " Ambas cartas destruidas." :
+                        (ganador == atacante && !perdedor.esInmortal() || atacante.toqueMortal() && defensor.getDefensa() < 2000 && !defensor.esInmortal())
+                        ? String.format(" Carta %s destruido/a.", defensor.getNombre()) :
+                        (defensor.toqueMortal() && atacante.getDefensa() < 2000 && !atacante.esInmortal())
+                        ? String.format(" Carta %s destruido/a.", atacante.getNombre())
+                        :"");
+            }
+        } else { // Es empate
+            resultado.append("Empate.");
+            if ((defensor.getPosicion() == Posicion.ATAQUE && !atacante.esInmortal()) || (atacante.toqueMortal() && defensor.toqueMortal() && defensor.getDefensa() < 2000 && atacante.getDefensa() < 2000)) {
+                resultado.append(" Ambas cartas destruidas.");
+            } else if (atacante.esInmortal()) {
+                resultado.append(String.format(" Carta %s destruido/a.", defensor.getNombre()));
+            }
+        }
+        return resultado.toString();
+    }
 
 }
 /*  Optional<Carta> ganador2 = Optional.ofNullable((puntosAtaque > puntosDefensor) ? atacante :
--- src/test/java/es/codeurjc/ais/CasoFinalTest.java
+++ src/test/java/es/codeurjc/ais/CasoFinalTest.java
@@ -7,13 +7,148 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CasoFinalTest {
     @Test
     void caso18() throws IllegalPositionException {
-        Carta uno = new Carta("uno", 1800, 2900, Posicion.ATAQUE, new Efecto[]{Efecto.TOQUE_MORTAL, Efecto.INMORTAL, Efecto.PRESION});
+        Carta uno = new Carta("uno", 1800, 2900, Posicion.ATAQUE, new Efecto[]{Efecto.INMORTAL, Efecto.PRESION});
         Carta dos = new Carta("dos", 2000, 1500, Posicion.ATAQUE);
 
-        String resultado = Combate.combatir(uno, dos); //arg1: atacante, arg2: defenso
-        String resultadoEsperado = "Carta uno (1800/2900/Posición: Ataque/Efectos: Toque mortal, Inmortal, Presión) vs Carta dos (2000/1500/Posición: Ataque/Efectos: N/A) -> " +
-                "Gana Carta dos. Atacante pierde 200 puntos.";
+        String resultado = Combate.combatir(uno, dos);
+        String resultadoEsperado = "Carta uno (1800/2900/Posición: Ataque/Efectos: Inmortal, Presión) vs Carta dos (2000/1500/Posición: Ataque/Efectos: N/A) -> " +
+                "Gana Carta dos. Atacante pierde 100 puntos.";
 
         assertEquals(resultadoEsperado, resultado);
     }
+
+    @Test
+    void caso19() throws IllegalPositionException {
+        Carta uno = new Carta("uno", 1900, 2000, Posicion.ATAQUE, new Efecto[]{Efecto.PRESION});
+        Carta dos = new Carta("dos", 3000, 1000, Posicion.DEFENSA, new Efecto[]{Efecto.TOQUE_MORTAL});
+
+        String resultado = Combate.combatir(uno, dos);
+        String resultadoEsperado = "Carta uno (1900/2000/Posición: Ataque/Efectos: Presión) vs Carta dos (3000/1000/Posición: Defensa/Efectos: Toque mortal) -> " +
+                "Gana Carta uno. Carta dos destruido/a.";
+
+        assertEquals(resultadoEsperado, resultado);
+    }
+    @Test
+    void caso20() throws IllegalPositionException {
+        Carta uno = new Carta("uno", 1900, 2000, Posicion.ATAQUE, new Efecto[]{Efecto.PRESION});
+        Carta dos = new Carta("dos", 3000, 1000, Posicion.DEFENSA, new Efecto[]{Efecto.TOQUE_MORTAL, Efecto.INMORTAL});
+
+        String resultado = Combate.combatir(uno, dos);
+        String resultadoEsperado = "Carta uno (1900/2000/Posición: Ataque/Efectos: Presión) vs Carta dos (3000/1000/Posición: Defensa/Efectos: Toque mortal, Inmortal) -> " +
+                "Gana Carta uno.";
+
+        assertEquals(resultadoEsperado, resultado);
+    }
+    @Test
+    void caso21() throws IllegalPositionException {
+        Carta uno = new Carta("uno", 1900, 1800, Posicion.ATAQUE, new Efecto[]{Efecto.PRESION});
+        Carta dos = new Carta("dos", 3000, 1000, Posicion.DEFENSA, new Efecto[]{Efecto.TOQUE_MORTAL, Efecto.INMORTAL});
+
+        String resultado = Combate.combatir(uno, dos);
+        String resultadoEsperado = "Carta uno (1900/1800/Posición: Ataque/Efectos: Presión) vs Carta dos (3000/1000/Posición: Defensa/Efectos: Toque mortal, Inmortal) -> " +
+                "Gana Carta uno. Carta uno destruido/a.";
+
+        assertEquals(resultadoEsperado, resultado);
+    }
+
+   @Test
+    void caso22() throws IllegalPositionException {
+        Carta uno = new Carta("uno", 1900, 1800, Posicion.ATAQUE, new Efecto[]{Efecto.PRESION, Efecto.INMORTAL});
+        Carta dos = new Carta("dos", 3000, 1000, Posicion.DEFENSA, new Efecto[]{Efecto.TOQUE_MORTAL});
+
+        String resultado = Combate.combatir(uno, dos);
+        String resultadoEsperado = "Carta uno (1900/1800/Posición: Ataque/Efectos: Presión, Inmortal) vs Carta dos (3000/1000/Posición: Defensa/Efectos: Toque mortal) -> " +
+                "Gana Carta uno. Carta dos destruido/a.";
+
+        assertEquals(resultadoEsperado, resultado);
+    }
+    @Test
+    void caso23() throws IllegalPositionException {
+        Carta uno = new Carta("uno", 600, 1800, Posicion.ATAQUE, new Efecto[]{Efecto.TOQUE_MORTAL, Efecto.INMORTAL});
+        Carta dos = new Carta("dos", 3000, 1000, Posicion.ATAQUE, new Efecto[]{Efecto.INMORTAL});
+
+        String resultado = Combate.combatir(uno, dos);
+        String resultadoEsperado = "Carta uno (600/1800/Posición: Ataque/Efectos: Toque mortal, Inmortal) vs Carta dos (3000/1000/Posición: Ataque/Efectos: Inmortal) -> " +
+                "Gana Carta dos. Atacante pierde 2400 puntos.";
+
+        assertEquals(resultadoEsperado, resultado);
+    }
+    @Test
+    void caso24() throws IllegalPositionException {
+        Carta uno = new Carta("uno", 600, 1800, Posicion.ATAQUE, new Efecto[]{Efecto.TOQUE_MORTAL});
+        Carta dos = new Carta("dos", 3000, 1000, Posicion.ATAQUE, new Efecto[]{Efecto.INMORTAL});
+
+        String resultado = Combate.combatir(uno, dos);
+        String resultadoEsperado = "Carta uno (600/1800/Posición: Ataque/Efectos: Toque mortal) vs Carta dos (3000/1000/Posición: Ataque/Efectos: Inmortal) -> " +
+                "Gana Carta dos. Atacante pierde 2400 puntos. Carta uno destruido/a.";
+
+        assertEquals(resultadoEsperado, resultado);
+    }
+    @Test
+    void caso25() throws IllegalPositionException {
+        Carta uno = new Carta("uno", 6000, 1800, Posicion.ATAQUE, new Efecto[]{Efecto.TOQUE_MORTAL});
+        Carta dos = new Carta("dos", 3000, 1000, Posicion.ATAQUE, new Efecto[]{Efecto.INMORTAL});
+
+        String resultado = Combate.combatir(uno, dos);
+        String resultadoEsperado = "Carta uno (6000/1800/Posición: Ataque/Efectos: Toque mortal) vs Carta dos (3000/1000/Posición: Ataque/Efectos: Inmortal) -> " +
+                "Gana Carta uno. Defensor pierde 3000 puntos.";
+
+        assertEquals(resultadoEsperado, resultado);
+    }
+    @Test
+    void caso26() throws IllegalPositionException {
+        Carta uno = new Carta("uno", 6000, 1800, Posicion.ATAQUE, new Efecto[]{Efecto.TOQUE_MORTAL});
+        Carta dos = new Carta("dos", 3000, 1000, Posicion.ATAQUE, new Efecto[]{Efecto.INMORTAL, Efecto.PRESION});
+
+        String resultado = Combate.combatir(uno, dos);
+        String resultadoEsperado = "Carta uno (6000/1800/Posición: Ataque/Efectos: Toque mortal) vs Carta dos (3000/1000/Posición: Ataque/Efectos: Inmortal, Presión) -> " +
+                "Gana Carta uno. Defensor pierde 1500 puntos.";
+
+        assertEquals(resultadoEsperado, resultado);
+    }
+    @Test
+    void caso27() throws IllegalPositionException {
+        Carta uno = new Carta("uno", 6000, 1800, Posicion.ATAQUE, new Efecto[]{Efecto.TOQUE_MORTAL, Efecto.PRESION, Efecto.INMORTAL});
+        Carta dos = new Carta("dos", 3000, 1000, Posicion.ATAQUE, new Efecto[]{Efecto.INMORTAL, Efecto.PRESION, Efecto.TOQUE_MORTAL});
+
+        String resultado = Combate.combatir(uno, dos);
+        String resultadoEsperado = "Carta uno (6000/1800/Posición: Ataque/Efectos: Toque mortal, Presión, Inmortal) vs Carta dos (3000/1000/Posición: Ataque/Efectos: Inmortal, Presión, Toque mortal) -> " +
+                "Gana Carta uno. Defensor pierde 1500 puntos.";
+
+        assertEquals(resultadoEsperado, resultado);
+    }
+    @Test
+    void caso28() throws IllegalPositionException {
+        Carta uno = new Carta("uno", 3000, 1800, Posicion.ATAQUE, new Efecto[]{Efecto.TOQUE_MORTAL, Efecto.PRESION, Efecto.INMORTAL});
+        Carta dos = new Carta("dos", 6000, 1000, Posicion.ATAQUE, new Efecto[]{Efecto.INMORTAL, Efecto.PRESION, Efecto.TOQUE_MORTAL});
+
+        String resultado = Combate.combatir(uno, dos);
+        String resultadoEsperado = "Carta uno (3000/1800/Posición: Ataque/Efectos: Toque mortal, Presión, Inmortal) vs Carta dos (6000/1000/Posición: Ataque/Efectos: Inmortal, Presión, Toque mortal) -> " +
+                "Gana Carta dos. Atacante pierde 1500 puntos.";
+
+        assertEquals(resultadoEsperado, resultado);
+    }
+    @Test
+    void caso29() throws IllegalPositionException {
+        Carta uno = new Carta("uno", 6000, 1800, Posicion.ATAQUE, new Efecto[]{Efecto.TOQUE_MORTAL, Efecto.PRESION, Efecto.INMORTAL});
+        Carta dos = new Carta("dos", 3000, 1000, Posicion.DEFENSA, new Efecto[]{Efecto.INMORTAL, Efecto.PRESION, Efecto.TOQUE_MORTAL});
+
+        String resultado = Combate.combatir(uno, dos);
+        String resultadoEsperado = "Carta uno (6000/1800/Posición: Ataque/Efectos: Toque mortal, Presión, Inmortal) vs Carta dos (3000/1000/Posición: Defensa/Efectos: Inmortal, Presión, Toque mortal) -> " +
+                "Gana Carta uno. Defensor pierde 1500 puntos.";
+
+        assertEquals(resultadoEsperado, resultado);
+    }
+    @Test
+    void caso30() throws IllegalPositionException {
+        Carta uno = new Carta("uno", 3000, 1800, Posicion.ATAQUE, new Efecto[]{Efecto.TOQUE_MORTAL, Efecto.PRESION, Efecto.INMORTAL});
+        Carta dos = new Carta("dos", 6000, 1000, Posicion.DEFENSA, new Efecto[]{Efecto.INMORTAL, Efecto.PRESION, Efecto.TOQUE_MORTAL});
+
+        String resultado = Combate.combatir(uno, dos);
+        String resultadoEsperado = "Carta uno (3000/1800/Posición: Ataque/Efectos: Toque mortal, Presión, Inmortal) vs Carta dos (6000/1000/Posición: Defensa/Efectos: Inmortal, Presión, Toque mortal) -> " +
+                "Gana Carta dos. Atacante pierde 1500 puntos.";
+
+        assertEquals(resultadoEsperado, resultado);
+    }
+
 }
